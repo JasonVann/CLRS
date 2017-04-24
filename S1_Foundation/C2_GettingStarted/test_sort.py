58932ed86bc79f4b822afc9f2be63b3ecb4f2cf3
JasonVann <jasonvanet@gmail.com>
2017-04-24 22:52:35 +0800

    Added functions to generate data and verify sorting

---
+++ S1_Foundation/C2_GettingStarted/test_sort.py
@@ -0,0 +1,17 @@
+import random
+
+max_num = 1e6
+
+def gen_test(n = 1000):
+    
+    data = [random.randint(-max_num, max_num) for i in range(n)]
+    return data
+
+def verify_sort(data):
+    n = len(data)
+    for i in range(n-1):
+        if data[i] > data[i+1]:
+            return False
+
+    return True
+    
