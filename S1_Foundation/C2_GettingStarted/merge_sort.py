0918608a6f53a91407c13e9eb7e78b0b37511629
JasonVann <jasonvanet@gmail.com>
2017-05-10 11:06:11 +0800

    Added my implementation of merge_sort

--- S1_Foundation/C2_GettingStarted/merge_sort.py
+++ S1_Foundation/C2_GettingStarted/merge_sort.py
@@ -4,6 +4,7 @@ def merge(A, p, q, r):
     # Assumes A[p..q], A[q+1...r] are already sorted
     n1 = q - p + 1
     n2 = r - q
+    
     out = [0]*(n1+n2)
     i = p
     j = q + 1
@@ -29,11 +30,35 @@ def merge(A, p, q, r):
     # Copy back the sorted arry to original array
     for i in range(p, r+1):
         A[i] = out[i-p]
-    print(A)
+    #print(A)
 
-def test():
+def merge_clrs(A, p, q, r):
+    # Use sentinel items to get rid of checking leftover items from L or R
+    pass
+
+def merge_sort(A, p, r):
+    # A[p] is the left-most item we are interested in
+    # A[r] is the right-most item
+    n = r - p + 1
+    if n <= 1:
+        return 
+    elif n == 2:
+        if A[p] > A[r]:
+            A[p], A[r] = A[r], A[p]
+        return
+    mid = p + int((r-p)/2) # In python3, may be float so cast to int
+    merge_sort(A, p, mid)
+    merge_sort(A, mid+1, r)
+    merge(A, p, mid, r)
+    return
+        
+def test_merge():
     A = [9,1,3,5,7,9,11,13,2,4,6,8,10]
     merge(A, 1, 7, 12)
-    
+
+def test_merge_sort(n = 1000):
+    data = gen_test(n)
+    merge_sort(data, 0, n-1)
+    print(verify_sort(data))
 
     
