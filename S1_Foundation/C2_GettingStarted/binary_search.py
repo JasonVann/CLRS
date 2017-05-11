e5ab54c8f1c277c0b38a50c6bdccff6af8838f79
JasonVann <jasonvanet@gmail.com>
2017-05-11 18:18:53 +0800

    Added iterative version of binary search

--- S1_Foundation/C2_GettingStarted/binary_search.py
+++ S1_Foundation/C2_GettingStarted/binary_search.py
@@ -3,6 +3,7 @@ from merge_sort import *
 
 def binary_search_recur(A, x, l, r):
     # Try to find x in sorted list A recursively
+    # l is the left-most idx, r is the right-most idx
     # Return the index of x if found, return False otherwise
     if l > r:
         return False
@@ -14,13 +15,28 @@ def binary_search_recur(A, x, l, r):
     else:
         return binary_search_recur(A, x, l, mid-1)
 
+def binary_search_iter(A, x):
+    # Iterative version of binary search
+    l = 0
+    r = len(A) - 1
+    while l <= r:
+        mid = int(l + (r-l)/2)
+        if A[mid] < x:
+            l = mid + 1
+        elif A[mid] == x:
+            return mid
+        else:
+            r = mid - 1
+    return False
+
 def one_test(n = 1000):
     # Generate a single test for binary search
     max_num = 2e3
     A = gen_test(n, max_num)
     merge_sort(A, 0, n-1)
     a = random.randint(-max_num, max_num)
-    idx = binary_search_recur(A, a, 0, n-1)
+    #idx = binary_search_recur(A, a, 0, n-1)
+    idx = binary_search_iter(A, a)
     #print(a, idx)
     if not idx and a not in A:
         #print("Not found and correct")
@@ -32,7 +48,7 @@ def one_test(n = 1000):
         #print("Something went wrong!")
         return False
 
-def many_tests(count = 10):
+def many_tests(count = 100):
     # Repeated calls one_test to verify binary search
     list_len = 1000
     for i in range(count):
--- S1_Foundation/C2_GettingStarted/test_sort.py
+++ S1_Foundation/C2_GettingStarted/test_sort.py
@@ -2,15 +2,16 @@ import random
 
 max_num = 1e6
 
-def gen_test(n = 1000):
+def gen_test(n = 1000, max_num = max_num):
+    # Generate a list of n items randomly, each number is within +/- of max_num
     n = int(n)
     data = [random.randint(-max_num, max_num) for i in range(n)]
     return data
 
 def verify_sort(data):
+    # Verify that the data is sorted ascendingly
     n = len(data)
     for i in range(n-1):
         if data[i] > data[i+1]:
             return False
-
     return True
