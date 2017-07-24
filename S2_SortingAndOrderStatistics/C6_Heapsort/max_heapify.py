24818fb0a61d2d4c041f67970df7322f2c8b78d8
JasonVann <jasonvanet@gmail.com>
2017-07-24 22:09:25 +0800

    Added recursive max_heapify for Ex6.2-5

--- S2_SortingAndOrderStatistics/C6_Heapsort/max_heapify.py
+++ S2_SortingAndOrderStatistics/C6_Heapsort/max_heapify.py
@@ -21,11 +21,35 @@ def max_heapify(A:Heap, i):
         A.data[largest], A.data[i] = A.data[i], A.data[largest]
         max_heapify(A, largest)
 
+def max_heapify_recur(A:Heap, i):
+    '''
+    A recursive version of max_heapify
+    For Ex6.2-5
+    '''
+    if i >= A.size or i < 0:
+        raise "i is not a valid index"
+
+    while i < A.size:
+        left = A.left(i)
+        right = A.right(i)
+        largest = i
+        if left is not None and A.data[left] >= A.data[i]:
+            largest = left
+        if right is not None and A.data[right] >= A.data[largest]:
+            largest = right
+        if i != largest:
+            # Then max heap property is violated
+            A.data[largest], A.data[i] = A.data[i], A.data[largest]
+            i = largest
+        else:
+            break
+
 def test_max_heapify():
     A = [16, 4, 10, 14, 7, 9, 3, 2, 8, 1]
     heap = Heap.Heap(A)
     print(heap.data)
-    max_heapify(heap, 1)
+    #max_heapify_recur(heap, 1)
+    max_heapify_recur(heap, 1)
     print(heap.data)
 
-#test_max_heapify()
+test_max_heapify()
