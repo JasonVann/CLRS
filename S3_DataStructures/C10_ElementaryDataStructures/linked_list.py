434de99309deaae5f736bff89a10b60349a33bd5
JasonVann <jasonvanet@gmail.com>
2017-08-16 23:39:59 +0800

    Added linked list

---
+++ S3_DataStructures/C10_ElementaryDataStructures/linked_list.py
@@ -0,0 +1,64 @@
+class Node():
+    def __init__(self, val):
+        self.prev = None
+        self.next = None
+        self.val = val
+
+    def __str__(self):
+        res = ''
+        if self.prev:
+            res += str(self.prev) + ', '
+        res += str(self.val)
+        return res
+
+class Linked_List():
+    def __init__(self, head = None):
+        if head:
+            self.head = Node(head)
+        else:
+            self.head = None
+
+    def search(self, v):
+        # Search for node with val = v
+        # Returns None if not found
+        x = self.head
+        while x and x.val != v:
+            x = x.next
+        return x
+
+    def insert(self, x):
+        # Insert value x to the front of the list
+        head = self.head
+        new_head = Node(x)
+        new_head.next = head
+        if head:
+            head.prev = new_head
+        self.head = new_head
+
+    def delete(self, v):
+        # Delete value v from the list
+        node = self.search(v)
+        if node:
+            prev = node.prev
+            next_node = node.next
+            if prev:
+                prev.next = next_node
+            else:
+                # v is the head
+                self.head = next_node
+            if next_node:
+                next_node.prev = prev
+
+def test():
+    ll = Linked_List(1)
+    ll.insert(4)
+    ll.insert(16)
+    ll.insert(9)
+    res = ll.search(1)
+    print(res)
+    ll.delete(4)
+    res = ll.search(1)
+    print(res)
+    print(ll.head)
+
+test()
